08eb82c98626524dc03aef91314dae6f49092343
CyberCaban <andrewgamer63@gmail.com>
2024-07-06 19:12:51 +0300

    added base path to vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(async () => ({
   plugins: [react()],
 
+  base: "/my-experience/",
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
